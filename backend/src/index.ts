7bc2140ee27ca5c948ef3c4ce0dfa2e7393f40d3
amartripathi0 <theamartripathi@gmail.com>
2024-09-30 21:45:21 +0530

    fix: add cors origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,22 @@
 import { Hono } from "hono";
 import { userRouter } from "./routes/user";
 import { blogRouter } from "./routes/blog";
+import { cors } from "hono/cors";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
-  }
+    FROTEND_URL: string;
+  };
 }>();
-
+app.use("/*", async (c, next) => {
+  const corsMiddlewareHandler = cors({
+    origin: c.env.FROTEND_URL,
+    credentials: true,
+  });
+  return corsMiddlewareHandler(c, next);
+});
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 
