55f438c9a01197aee4199be687c5cd74820cf5d9
amartripathi0 <theamartripathi@gmail.com>
2024-10-06 23:51:28 +0530

    fix: user page

--- frontend/src/pages/user.tsx
+++ frontend/src/pages/user.tsx
@@ -1,14 +1,15 @@
 import BlogsPage from "@/components/blogs";
-import { homepageBlogPosts } from "@/lib/constant";
 import { BlogType } from "@amartripathi/blog-types";
 import axios from "axios";
 import { Loader } from "lucide-react";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 
 function User() {
-  const [blogs, setBlogs] = useState<BlogType[]>();
+  const [blogs, setBlogs] = useState<BlogType[]>([]);
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+  const navigate = useNavigate();
 
   async function getUsersBlogs() {
     try {
@@ -19,22 +20,32 @@ function User() {
         },
       });
 
-      setBlogs(response.data?.blogs);
-    } catch {
-      toast.error("Internal Server Error");
+      setBlogs(response.data?.blogs || []);
+    } catch (error) {
+      const errorMessage = error?.response?.data || "An error occurred";
+      toast.error(`${errorMessage}, Please signin`);
+      navigate('/signin');
     }
   }
+
   useEffect(() => {
     getUsersBlogs();
   }, []);
+
   return blogs ? (
+    blogs.length > 0 ?
     <BlogsPage
-      blogsArray={homepageBlogPosts}
+      blogsArray={blogs}
       title="My Blogs"
       pageType="userBlogPage"
     />
+    :
+    <div className="flex flex-col gap-5 h-screen justify-center items-center bg-neutral-900 text-blue-400 text-xl">
+      <p> You don't have a blog. Add a new blog..</p>
+    </div>
+
   ) : (
-    <div className="flex flex-col gap-5 h-screen justify-center items-center bg-neutral-900  text-blue-400 text-xl">
+    <div className="flex flex-col gap-5 h-screen justify-center items-center bg-neutral-900 text-blue-400 text-xl">
       <p> Fetching Your Blogs...</p>
       <Loader className="animate-spin" size={40} />
     </div>
